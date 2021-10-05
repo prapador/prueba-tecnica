c35c177ff0a44893770f289be767f3c677a5b2c5
Sergio Martínez <sergiotr94@gmail.com>
2021-10-05 18:30:31 +0200

    Inicio

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main container">
+      <h2>Hola Humano,</h2>
+      <p>
+        Mi nombre es LOM. Hoy tienes una oportunidad para recuperar la raza
+        humana. ¿Estás preparado?
+      </p>
+      <router-link to="/questions" class="btn btn-success">
+        Vamos
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
