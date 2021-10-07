8154c3d14bf0a6111e91176935b121c97e605643
Sergio Martínez <sergiotr94@gmail.com>
2021-10-07 13:08:27 +0200

    gh-pages commit

---
+++ precache-manifest.ddb72579abdb8b694fa43224dcf8731d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bacb7e02d26e538c0ce",
+    "url": "/prueba-tecnica/css/about.c565ed92.css"
+  },
+  {
+    "revision": "81a87bcd1e8169737b42",
+    "url": "/prueba-tecnica/css/app.d5efdf0f.css"
+  },
+  {
+    "revision": "d96452babc15eb1c7b4b8b57ebef2a34",
+    "url": "/prueba-tecnica/index.html"
+  },
+  {
+    "revision": "9bacb7e02d26e538c0ce",
+    "url": "/prueba-tecnica/js/about.e561763f.js"
+  },
+  {
+    "revision": "81a87bcd1e8169737b42",
+    "url": "/prueba-tecnica/js/app.b7b04211.js"
+  },
+  {
+    "revision": "e9622b45eff00e3495c1",
+    "url": "/prueba-tecnica/js/chunk-vendors.2c08d1c9.js"
+  },
+  {
+    "revision": "bf11d20cb502933729edbe1a1b0fcaeb",
+    "url": "/prueba-tecnica/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/prueba-tecnica/robots.txt"
+  }
+]);
